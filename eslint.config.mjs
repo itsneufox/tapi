95d7ec6a201f4fd0246c48cfb96599667c395c73
itsneufox <156133096+itsneufox@users.noreply.github.com>
2025-04-05 19:18:49 +0100

    add ESLint configuration for TS

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default [
+  eslint.configs.recommended,
+  
+  ...tseslint.configs.recommended,
+  
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      }],
+    }
+  },
+  {
+    ignores: [
+      '**/dist/**',
+      '**/node_modules/**',
+    ]
+  }
+];
