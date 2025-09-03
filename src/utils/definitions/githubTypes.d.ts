a2bf0bd839b69e8e6f770dfbc5f4f86ebb63781a
Leamir <leamir@devmiranda.com.br>
2025-09-02 21:57:28 -0300

    Fix github types

--- src/utils/definitions/githubTypes.d.ts
+++ src/utils/definitions/githubTypes.d.ts
@@ -1,127 +1,49 @@
 interface GithubFileContentResponse {
   // Thanks api gods https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28&versionId=free-pro-team%40latest&restPage=authenticating-to-the-rest-api#get-repository-content
-  type: {
-    type: 'string';
-  };
-  size: {
-    type: 'integer';
-  };
-  name: {
-    type: 'string';
-  };
-  path: {
-    type: 'string';
-  };
-  sha: {
-    type: 'string';
-  };
-  content: {
-    type: 'string';
-  };
-  url: {
-    type: 'string';
-    format: 'uri';
-  };
-  git_url: {
-    type: ['string', 'null'];
-    format: 'uri';
-  };
-  html_url: {
-    type: ['string', 'null'];
-    format: 'uri';
-  };
-  download_url: {
-    type: ['string', 'null'];
-    format: 'uri';
-  };
+  type: string;
+  size: integer;
+  name: string;
+  path: string;
+  sha: string;
+  content: string;
+  url: string; //format: 'uri';
+  git_url: string | null;
+  html_url: string | null;
+  download_url: string | null;
   entries: {
-    type: 'array';
-    items: {
-      type: 'object';
-      properties: {
-        type: {
-          type: 'string';
-        };
-        size: {
-          type: 'integer';
-        };
-        name: {
-          type: 'string';
-        };
-        path: {
-          type: 'string';
-        };
-        sha: {
-          type: 'string';
-        };
-        url: {
-          type: 'string';
-          format: 'uri';
-        };
-        git_url: {
-          type: ['string', 'null'];
-          format: 'uri';
-        };
-        html_url: {
-          type: ['string', 'null'];
-          format: 'uri';
-        };
-        download_url: {
-          type: ['string', 'null'];
-          format: 'uri';
-        };
-        _links: {
-          type: 'object';
-          properties: {
-            git: {
-              type: ['string', 'null'];
-              format: 'uri';
-            };
-            html: {
-              type: ['string', 'null'];
-              format: 'uri';
-            };
-            self: {
-              type: 'string';
-              format: 'uri';
-            };
-          };
-          required: ['git', 'html', 'self'];
-        };
-      };
-      required: [
-        '_links',
-        'git_url',
-        'html_url',
-        'download_url',
-        'name',
-        'path',
-        'sha',
-        'size',
-        'type',
-        'url',
-      ];
+    type: string;
+    size: integer;
+    name: string;
+    path: string;
+    sha: string;
+    url: string; //format: 'uri';
+    git_url: string | null; //format: 'uri';
+    html_url: string | null; //format: 'uri';
+    download_url: string | null; //format: 'uri';
+    _links: {
+      git: string | null; //format: 'uri';
+      html: string | null; //format: 'uri';
+      self: string; //format: 'uri';
     };
-  };
-  encoding: {
-    type: 'string';
-  };
+    required: ['git', 'html', 'self'];
+  }[];
+  encoding: string;
   _links: {
     type: 'object';
     properties: {
-      git: {
-        type: ['string', 'null'];
-        format: 'uri';
-      };
-      html: {
-        type: ['string', 'null'];
-        format: 'uri';
-      };
-      self: {
-        type: 'string';
-        format: 'uri';
-      };
+      git: string | null; //format: 'uri';
+      html: string | null; //format: 'uri';
+      self: string; //format: 'uri';
     };
     required: ['git', 'html', 'self'];
+  }[];
+  encoding: string;
+  _links: {
+    type: 'object';
+    properties: {
+      git: string | null; //format: 'uri';
+      html: string | null; //format: 'uri';
+      self: string; //format: 'uri';
+    };
   };
 }
